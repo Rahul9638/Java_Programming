f3f2f7d0da81e09c1f65624952cd9ba40701d27b
rahul <rahul.paswan.9638@gmail.com>
2021-03-20 08:47:16 +0530

    singleton

---
+++ singletons/MainClass.java
@@ -0,0 +1,13 @@
+package singletons;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+//		AppConfig obj = new AppConfig();
+//		AppConfig obj2 = new AppConfig();
+		AppConfig obj = new AppConfig.getInstance();
+		
+
+	}
+
+}
