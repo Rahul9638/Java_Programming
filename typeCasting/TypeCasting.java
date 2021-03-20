d164d84249888a968ed396bb1d576b95d71ccc58
rahul <rahul.paswan.9638@gmail.com>
2021-03-20 08:50:25 +0530

    type Casting

---
+++ typeCasting/TypeCasting.java
@@ -0,0 +1,18 @@
+package typeCasting;
+
+public class TypeCasting {
+
+	public static void main(String[] args) {
+//		byte x = 3;
+//		int y = x;
+//		float z = y;
+//		System.out.println(y);
+//		System.out.println(z);
+		
+		double myDouble = 2.332232;
+		int myInt = (int)myDouble;
+		System.out.println(myDouble);
+		System.out.println(myInt);
+	}
+
+}
