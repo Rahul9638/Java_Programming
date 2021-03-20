547978894c5730b9613eba709e1624baf41e812f
rahul <rahul.paswan.9638@gmail.com>
2021-03-20 08:30:13 +0530

    Adding Datatypes

---
+++ Data_Types/DataTypesTutorial.java
@@ -0,0 +1,20 @@
+package Data_Types;
+
+public class DataTypesTutorial {
+
+	public static void main(String[] args) {
+		byte x = 2;
+		int age = 3;
+		float rateOfIntrest = 32.3f;
+		double rate = 33.23232;
+		boolean isThisSeriesCool = true;
+		char myCharacter = 'r';
+		System.out.println(x);
+		System.out.println(rateOfIntrest);
+		System.out.println(rate);
+		System.out.println(myCharacter);
+		System.out.println(isThisSeriesCool);
+
+	}
+
+}
