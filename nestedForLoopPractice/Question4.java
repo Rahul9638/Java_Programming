14ad3007e2ae66cae71563b983c29fc031add1f3
rahul <rahul.paswan.9638@gmail.com>
2021-03-20 08:35:50 +0530

    nested for loop practice

---
+++ nestedForLoopPractice/Question4.java
@@ -0,0 +1,21 @@
+package nestedForLoopPractice;
+import java.util.Scanner;
+public class Question4 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		for (int i=1; i<=n; i++) {
+			
+			for (int j= 1; j<=1; j++) {
+				System.out.print(" *");
+			}
+			for (int j = 1; j<=n-1; j++) {
+				System.out.print("");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
